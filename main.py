d2a7878ad23fe01770e1e63e7bef57b5d0eff80f
Bratenkov Miron <brat_miron@mail.ru>
2023-10-15 17:08:58 +0700

    fix

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def generator(data, batch_size, shuffle=False):
 
 
 # raw_datasets_val = load_dataset('json', data_files={'train': ['eval.txt']})['train'].select(range(100))
-raw_datasets_train = load_dataset("wmt19", "ru-en", split='train[:1_000_000]')
+raw_datasets_train = load_dataset("wmt19", "ru-en", split='train[:1000000]')
 raw_datasets_val = load_dataset('json', data_files={'train': ['eval.txt']})['train']
 datasets_train = raw_datasets_train.map(preprocess_function, batched=True)
 datasets_val = raw_datasets_val.map(preprocess_function, batched=True)
