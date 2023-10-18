dd8f487bcc0c64df55b3c9e0e3d6ef1005190ef4
Bratenkov Miron <brat_miron@mail.ru>
2023-10-18 14:36:22 +0700

    for train

--- main.py
+++ main.py
@@ -91,13 +91,12 @@ raw_datasets_val = load_dataset('json', data_files={'train': ['eval.txt']})['tra
 datasets_train = raw_datasets_train.map(preprocess_function, batched=True)
 datasets_val = raw_datasets_val.map(preprocess_function, batched=True)
 train_inputs = np.array(datasets_train['input_ids'], dtype=object)
-print(train_inputs[:10])
-l = np.arange([len(i) for i in train_inputs])
+l = np.arange(len(train_inputs))
 train_inputs = train_inputs[l < 150]
 train_targets = np.array(datasets_train['labels'], dtype=object)
 train_targets = train_targets[l < 150]
 val_inputs = np.array(datasets_val['input_ids'], dtype=object)
-l = np.arange([len(i) for i in val_inputs])
+l = np.arange(len(val_inputs))
 val_inputs = val_inputs[l < 150]
 val_targets = np.array(datasets_val['labels'], dtype=object)
 val_targets = val_targets[l < 150]
