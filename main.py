51a508dc07bd815b776c0a977accc2a139e947b9
Bratenkov Miron <brat_miron@mail.ru>
2023-11-06 23:43:09 +0700

    clustering

--- main.py
+++ main.py
@@ -124,7 +124,8 @@ tokenizer_cluster = BertTokenizer.from_pretrained('bert-base-multilingual-cased'
 model_cluster = BertModel.from_pretrained("bert-base-multilingual-cased")
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='pt')
-output = model(encoded_input)
+print(encoded_input)
+output = model(**encoded_input)
 print(output.shape)
 
 # raw_datasets_val = load_dataset('json', data_files={'train': ['eval.txt']})['train'].select(range(100))
