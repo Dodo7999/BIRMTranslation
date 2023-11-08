50ba0d7019bbd2010563ee2ec0e0ee205ebded2f
Bratenkov Miron <brat_miron@mail.ru>
2023-11-08 14:18:14 +0700

    clustering

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@
 #SBATCH --output=/userspace/bma/bsl_shift.log
 #SBATCH --partition=a100
 #SBATCH --nodes=1
-#SBATCH --nodelist=ngpu05
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-socket=1
