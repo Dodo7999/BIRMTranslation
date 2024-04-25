15f7f8f76e1e446884c56e4a5471dec7301e4e67
Miron Bratenkov <m.bratenkov@g.nsu.ru>
2024-04-26 00:48:12 +0700

    nlp new pretraining

--- pretraining/something_max.py
+++ pretraining/something_max.py
@@ -301,7 +301,7 @@ val_targets = val_set[:, 1]
 test_inputs = test_set[:, 0]
 test_targets = test_set[:, 1]
 gc.collect()
-n_epoch = 1
+n_epoch = 3
 cel = torch.nn.CrossEntropyLoss()
 opt = torch.optim.AdamW(model.parameters(), lr=5e-4)
 
