6e67909922fe7964223196b13588c1338138549e
Miron Bratenkov <m.bratenkov@g.nsu.ru>
2024-06-21 17:11:03 +0700

    nlp new pretraining

--- start_birm_mid.sh
+++ start_birm_mid.sh
@@ -31,6 +31,7 @@ export PATH=/usr/local/cuda-11/bin${PATH:+:${PATH}}
 nvidia-smi -L
 nvcc -V
 python -V
+pip install /userspace/bma/AutoGPTQ-main.zip
 python -c "import torch, transformers, datasets, tokenizers; print(f'torch.version = {torch.__version__}, CUDA = {torch.cuda.is_available()}, transformers.version = {transformers.__version__}, datasets.version = {datasets.__version__}, tokenizers.version = {tokenizers.__version__}')"
 
 python -u pretraining/something_mid.py
