968848f0098407c055949286f6413f877ddb2b1c
Miron Bratenkov <m.bratenkov@g.nsu.ru>
2025-01-27 00:42:24 +0700

    nlp new pretraining

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#SBATCH --job-name=qwen_erm_translate
-#SBATCH --error=/userspace/bma/qwen_erm_translate_err.log
-#SBATCH --output=/userspace/bma/qwen_erm_translate.log
+#SBATCH --job-name=qwen_erm_paraphrase
+#SBATCH --error=/userspace/bma/qwen_erm_paraphrase.log
+#SBATCH --output=/userspace/bma/qwen_erm_paraphrase.log
 #SBATCH --partition=a100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-socket=1
 #SBATCH --no-requeue
-#SBATCH -o qwen_erm_translate.log
+#SBATCH -o qwen_erm_paraphrase.log
 
 echo "# SLURM_JOBID  = ${SLURM_JOBID}"
 echo "# SLURM_JOB_NODELIST = ${SLURM_JOB_NODELIST}"
